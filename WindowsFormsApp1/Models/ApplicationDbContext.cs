6173db4a6a8cfd39c4c95174627348b6f75a65a0
VietNguyenXuan <nguyenviet.06092000@gmail.com>
2023-01-07 19:11:26 +0700

    Demo

---
+++ WindowsFormsApp1/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+  internal class ApplicationDbContext:DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=D:\\McProtocol_EntityFrameworkCoreNet2_DatabaseCopy\\DB\\Database.db");
+    }
+    public DbSet<Student> Students { get; set; }
+  }
+}
